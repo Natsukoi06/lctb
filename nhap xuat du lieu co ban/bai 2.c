f13e6e16b9cde856367a9f24138f0585c0511644
Natsukoi06 <dhn221006@gmail.com>
2025-04-03 08:02:11 +0700

    Create bai 2.c

---
+++ nhap xuat du lieu co ban/bai 2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int dd, mm, yy;
+    printf("Nhap ngay thang nam: ");
+    scanf("%d%d%d", &dd, &mm, &yy);
+    printf("Hom nay la ngay %d/%d/%d", dd, mm, yy);
+    return 0;
+}
