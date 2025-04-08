bfcf5ba103d38fb9c970900950bbca3e726ae0d8
Natsukoi06 <dhn221006@gmail.com>
2025-04-08 08:21:45 +0700

    Create bai 6.c

---
+++ Mang 1 chieu/bai 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    freopen("D:\\cykablry\\input.txt", "r", stdin);
+    int a[1000];
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] < 0)
+        {
+            a[i] = 0;
+        }
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
