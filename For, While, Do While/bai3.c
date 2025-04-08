0ecd6d24f30c7a00b70e803802c20db71e576d64
Natsukoi06 <dhn221006@gmail.com>
2025-04-08 07:57:46 +0700

    Create bai3.c

---
+++ For, While, Do While/bai3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+//freopen("D:\\cykablry\\input.txt", "r", stdin);
+
+int main()
+{
+    for(int i = 1; i <= 20; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d\t", i);
+        }
+    }
+    return 0;
+}
