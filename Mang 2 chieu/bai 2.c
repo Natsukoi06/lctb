5d91c42ad2dcbaab143bb61c7465b02003ecd670
Natsukoi06 <dhn221006@gmail.com>
2025-04-14 19:32:54 +0700

    Create bai 2.c

---
+++ Mang 2 chieu/bai 2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define ROWS 100
+#define COLS 100
+
+void nhapMatrix(int a[][COLS], int m, int n)
+{
+    printf("Nhap ma tran: ");
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+}
+
+int main()
+{
+    int m, n;
+    printf("Nhap kich co ma tran:");
+    scanf("%d%d", &m, &n);
+    if(m > ROWS || n > COLS)
+    {
+        printf("Kich thuoc khong hop le!");
+        return 0;
+    }
+
+    int a[ROWS][COLS];
+    nhapMatrix(a, m, n);
+
+    int b[ROWS][COLS];
+    nhapMatrix(b, m, n);
+
+    int c[ROWS][COLS];
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("Tong 2 ma tran la:\n");
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("%d  ", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
