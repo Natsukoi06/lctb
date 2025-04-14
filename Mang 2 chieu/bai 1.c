81fb13638c4c2be753874e0d6868f4a3ad066def
Natsukoi06 <dhn221006@gmail.com>
2025-04-14 19:18:02 +0700

    Update bai 1.c

--- Mang 2 chieu/bai 1.c
+++ Mang 2 chieu/bai 1.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main()
+{
+    //Truong hop 1
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    printf("Ma tran da khai bao gia tri la:\n");
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //Truong hop 2
+    int b[3][3];
+    printf("Nhap ma tran kich co 3x3:\n");
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("Ma tran sau khi nhap la\n");
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
