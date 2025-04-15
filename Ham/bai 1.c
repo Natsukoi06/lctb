ba382e778b1c7d76b4641aea1b3cd57413d7bbab
Natsukoi06 <dhn221006@gmail.com>
2025-04-15 18:01:32 +0700

    Update bai 1.c

--- Ham/bai 1.c
+++ Ham/bai 1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+double power(double x, int y) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y < 0) {
+        return 1 / power(x, -y);
+    }
+    return x * power(x, y - 1);
+}
+
+int main() {
+    double x;
+    int y;
+
+    printf("Nhap x: ");
+    scanf("%lf", &x);
+    printf("Nhap y: ");
+    scanf("%d", &y);
+
+    printf("%.2lf^%d = %.4lf\n", x, y, power(x, y));
+
+    return 0;
+}
 
